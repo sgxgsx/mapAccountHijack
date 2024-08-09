1c77f19cc755a7aa610ff5ea032a270987b21e38
sgxgsx <vladosstrawberry@gmail.com>
2024-08-10 01:25:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ python3 -m pip install aiohttp
 git clone https://github.com/nccgroup/nOBEX.git
 cd nOBEX
 sudo python3 setup.py build
+sudo python3 setup.py install
+sudo -E env PATH=$PATH python3 setup.py build
 cd ..
 git clone https://github.com/thxomas/bdaddr 
 cd bdaddr
