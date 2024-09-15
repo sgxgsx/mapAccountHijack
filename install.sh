4bc907bcf2544699f43516463acccf1c38c6467d
sgxgsx <vladosstrawberry@gmail.com>
2024-09-15 02:41:16 +0200

    Fix installation errors install.sh
    Fix nOBEX not installing

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 python3 -m pip install aiohttp
 git clone https://github.com/nccgroup/nOBEX.git
 cd nOBEX
-sudo python3 setup.py build
-sudo python3 setup.py install
-sudo -E env PATH=$PATH python3 setup.py build
+#sudo python3 setup.py build
+#sudo python3 setup.py install
+#sudo -E env PATH=$PATH python3 setup.py build
+sudo -E env PATH=$PATH pip install --break-system-packages .
 cd ..
 git clone https://github.com/thxomas/bdaddr 
 cd bdaddr
